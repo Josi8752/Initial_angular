125005821585b72ada90e72e2b8ceec4b4ca7e1a
Josiane Silva <josianesouza8752@gmail.com>
2023-06-13 17:55:11 -0300

    My first componet

---
+++ primary-Project-Angular/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+    <p> Meu primeiro component com Angular!</p>
+    `
+})
+export class MeuPrimeiroComponent { }
