6f812fde838a849d0642df1613e556d2801b6770
Josiane Silva <josianesouza8752@gmail.com>
2023-06-16 10:46:41 -0300

    Understanding more Ts

---
+++ typescript/main.ts
@@ -0,0 +1,21 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x: any, y: any) {
+    return x + y;
+}
+
+
+//Es 6 ou Es 2015
+let num = 2015;
+const PI = 3.14;
+
+let numeros = [1, 2, 3];
+numeros.map(valor => valor * 2);
+
+class Mat {
+    soma(x, y) {
+        return x + y;
+    }
+}
+
+
